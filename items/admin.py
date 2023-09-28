c4f450fee6f998dde9cf294570673bafce1b3e40
Timbusien <timbusiennavi@gmail.com>
2023-09-29 00:38:29 +0500

    migrates

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from items.models import ProductModel
+from items.models import CategoryModel
 
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'uploaded_at']
+#
+#
+@admin.register(ProductModel)
+class ProductModeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'uploaded_at']
+    search_fields = ['title', 'price']
+    list_filter = ['uploaded_at']
 # Register your models here.
--- items/models.py
+++ items/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class CategoryModel(models.Model):
+    title = models.CharField(max_length=70)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class ProductModel(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    # category = models.ManyToManyField(CategoryModel, null=True, blank=True)
+    img = models.FileField(null=True, blank=True)
+    description = models.TextField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'продукты для магазина'
+        verbose_name_plural = 'добавление продуктов'
+
+
+
